10e3346615fe97cfb27deff45f42ef91e5461e2d
vamandeshmukh <87980161+shridhar-javafsd@users.noreply.github.com>
2022-01-31 15:27:20 +0530

    Update batch2_queries.sql

--- batch2_queries.sql
+++ batch2_queries.sql
@@ -38,6 +38,8 @@ SELECT eid, first_name, salary FROM emp ORDER BY first_name;
 
 SELECT eid, first_name, salary FROM emp WHERE salary > 50000 ORDER BY salary; 
 
+-- using functions 
+
 -- What is the max salary in the emp table? 
 SELECT max(salary) FROM emp;
 SELECT min(salary) FROM emp;
@@ -53,5 +55,49 @@ SELECT count(distinct(first_name)) FROM emp;
 
 SELECT 100 / 3;
 SELECT ROUND(100 / 3);
+SELECT ROUND((100 / 3), 1);
 SELECT ROUND((100 / 3), 2);
+
+SELECT first_name, salary, salary + 10000 FROM emp;
+SELECT first_name, salary, salary + 10000 'hike' FROM emp;
+
+-- DML operations - insert, update, delete 
+
+SELECT * FROM emp;
+
+-- insert a new record 
+INSERT INTO emp (eid, first_name, salary) VALUES (106, 'Monu', 65000);
+INSERT INTO emp VALUES (107, 'Sonu', 58000); -- shortcut 
+
+-- insert a record with null value in a column 
+INSERT INTO emp (eid, first_name, salary) VALUES (108, 'Monu', NULL);
+INSERT INTO emp (eid, first_name) VALUES (109, 'Monu');
+INSERT INTO emp (salary, eid) VALUES (75000, 110);
+INSERT INTO emp (eid, first_name, salary) VALUES (111, 'Ponu', 65000);
+
+-- update a record - UPDATE table_name SET column_name = new value WHERE search condition
+UPDATE emp SET salary = 25000 WHERE eid = 107;
+
+SELECT * FROM emp;
+
+-- delete a record - DELETE FROM table_name WHERE search condition
+DELETE FROM emp WHERE eid = 109;
+-- DELETE emp WHERE eid = 109; -- works in Oracle 
+
+-- create tables - 
+-- CREATE TABLE table_name (column_name1 datatype, column_name2 datatype, ...)
+CREATE TABLE cg_emps (eid INT, first_name VARCHAR(40), phone BIGINT, hire_date DATE, salary DOUBLE);
+
+INSERT INTO cg_emps (eid, first_name, phone, hire_date, salary) 
+VALUES(101, 'Prakhar', 9876543210, STR_TO_DATE('26-01-2022', '%d-%m-%Y'), 52000);
+
+INSERT INTO cg_emps (eid, first_name, phone, hire_date, salary) 
+VALUES(102, 'Suresh', 7485968945, current_date(), 52000);
+
+COMMIT;
+SELECT * FROM cg_emps;
+
+
+
+
 
